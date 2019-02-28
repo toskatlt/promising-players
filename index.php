807a32a906a40b31008c8e65d950bf59662d77e8
toskatlt <to_Ska@mail.ru>
2019-02-28 11:50:52 +0400

    Рабочая версия от 28.02.2019

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/* HEADER */ include ($_SERVER["DOCUMENT_ROOT"]."/section/header.php");
+/* MENU */  include ($_SERVER["DOCUMENT_ROOT"]."/section/menu.php");
+?>
+
+<div class='container'></div>
+
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/section/footer.php");
+
